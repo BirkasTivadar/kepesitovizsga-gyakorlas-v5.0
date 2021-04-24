5d766d8c3ebe892ff2f25bfc62895a6b7ac309cc
BirkasTivadar <birkastivadar@gmail.com>
2021-04-24 11:17:27 +0200

    Cinderella kész

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/cinderella/Cinderella.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/cinderella/Cinderella.java
@@ -1,5 +1,56 @@
 package hu.nive.ujratervezes.kepesitovizsga.cinderella;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Cinderella {
 
+    private int[] numbers = new int[5];
+
+    public int[] getNumbers() {
+        return numbers;
+    }
+
+    public void readFromFile() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+                Cinderella.class.getResourceAsStream("/cinderella/cinderella.txt")))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                for (String s : line.split(" ")) {
+                    if (s.contains("Hamupipőke")) {
+                        numbers[0]++;
+                    }
+                    if (s.toLowerCase().contains("galamb")) {
+                        numbers[1]++;
+                    }
+                    if (s.toLowerCase().contains("királyfi")) {
+                        numbers[2]++;
+                    }
+                    if (s.toLowerCase().contains("mostoha")) {
+                        numbers[3]++;
+                    }
+                    if (s.toLowerCase().contains("cipő")) {
+                        numbers[4]++;
+                    }
+                }
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+    }
+
+    public Object[] getMax() {
+        String[] temp = {"Hamupipőke", "galamb", "királyfi", "mostoha", "cipő"};
+        int max = numbers[0];
+        int maxIndex = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+                maxIndex = i;
+            }
+        }
+        Object[] result = {temp[maxIndex], numbers[maxIndex]};
+        return result;
+    }
 }
--- src/test/java/hu/nive/ujratervezes/kepesitovizsga/cinderella/CinderellaTest.java
+++ src/test/java/hu/nive/ujratervezes/kepesitovizsga/cinderella/CinderellaTest.java
@@ -1,29 +1,32 @@
 package hu.nive.ujratervezes.kepesitovizsga.cinderella;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CinderellaTest {
 
-//    private Cinderella cinderella;
-//
-//    @BeforeEach
-//    public void setUp() {
-//        cinderella = new Cinderella();
-//        cinderella.readFromFile();
-//    }
-//
-//    @Test
-//    public void readFromFile() {
-//        Assertions.assertEquals(12, cinderella.getNumbers()[1]);
-//        Assertions.assertEquals(22, cinderella.getNumbers()[3]);
-//    }
-//
-//    @Test
-//    public void getMax() {
-//        Object[] expected = cinderella.getMax();
-//
-//        Assertions.assertEquals("Hamupipőke", expected[0]);
-//        Assertions.assertEquals(32, expected[1]);
-//    }
+    private Cinderella cinderella;
+
+    @BeforeEach
+    public void setUp() {
+        cinderella = new Cinderella();
+        cinderella.readFromFile();
+    }
+
+    @Test
+    public void readFromFile() {
+        assertEquals(12, cinderella.getNumbers()[1]);
+        assertEquals(22, cinderella.getNumbers()[3]);
+    }
+
+    @Test
+    public void getMax() {
+        Object[] expected = cinderella.getMax();
+
+        assertEquals("Hamupipőke", expected[0]);
+        assertEquals(32, expected[1]);
+    }
 
 }

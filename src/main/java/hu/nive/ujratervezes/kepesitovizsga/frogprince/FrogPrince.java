25ed5d35d3275cb7b6eb863c179df0cf9f83f754
BirkasTivadar <birkastivadar@gmail.com>
2021-04-24 13:04:53 +0200

    FrogPrince kész

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/frogprince/FrogPrince.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/frogprince/FrogPrince.java
@@ -1,5 +1,39 @@
 package hu.nive.ujratervezes.kepesitovizsga.frogprince;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
+
 public class FrogPrince {
 
+    public Set<PlayerOfTale> getPlayersOfTale(DataSource dataSource, Tale tale) {
+        String sqlOrder = String.format("SELECT player_name, age, tale FROM tales WHERE tale='%s';", tale.name());
+        Set<PlayerOfTale> result = new HashSet<>();
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sqlOrder);
+        ) {
+            processResultSet(result, ps);
+        } catch (SQLException sqlException) {
+            throw new IllegalStateException("Connection failed", sqlException);
+        }
+        return result;
+    }
+
+    private void processResultSet(Set<PlayerOfTale> result, PreparedStatement ps) {
+        try (ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                String name = rs.getString("player_name");
+                int age = rs.getInt("age");
+                String title = rs.getString("tale");
+                result.add(new PlayerOfTale(name, age, Tale.valueOf(title)));
+            }
+        } catch (SQLException sqlException) {
+            throw new IllegalStateException("Cannot query", sqlException);
+        }
+    }
 }
--- src/test/java/hu/nive/ujratervezes/kepesitovizsga/frogprince/FrogPrinceTest.java
+++ src/test/java/hu/nive/ujratervezes/kepesitovizsga/frogprince/FrogPrinceTest.java
@@ -1,35 +1,40 @@
 package hu.nive.ujratervezes.kepesitovizsga.frogprince;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+import com.sun.source.doctree.SeeTree;
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FrogPrinceTest {
 
-//    private MariaDbDataSource dataSource;
-//
-//    @BeforeEach
-//    public void setUp() {
-//        try {
-//            dataSource = new MariaDbDataSource();
-//            dataSource.setUrl("jdbc:mariadb://localhost:3306/employees?useUnicode=true");
-//            dataSource.setUser("employees");
-//            dataSource.setPassword("employees");
-//        } catch (SQLException sqle) {
-//            throw new IllegalStateException("Can not get data.", sqle);
-//        }
-//
-//        Flyway fw = Flyway.configure().dataSource(dataSource).load();
-//        fw.clean();
-//        fw.migrate();
-//    }
-//
-//    @Test
-//    public void getPlayersOfTale() {
-//        Set<PlayerOfTale> playersOfTale = new FrogPrince().getPlayersOfTale(dataSource, Tale.FROGPRINCE);
-//
-//        Assertions.assertTrue(playersOfTale.contains(new PlayerOfTale("Békakirályfi", 27, Tale.FROGPRINCE)));
-//        Assertions.assertTrue(playersOfTale.contains(new PlayerOfTale("Kicsi királykisasszony", 21, Tale.FROGPRINCE)));
-//        Assertions.assertFalse(playersOfTale.contains(new PlayerOfTale("Hófehérke", 23, Tale.SNOWWHITE)));
-//        Assertions.assertFalse(playersOfTale.contains(new PlayerOfTale("Piroska", 9, Tale.LITTLEREDRIDINGHOOD)));
-//    }
+    MysqlDataSource dataSource = new MysqlDataSource();
+
+    @BeforeEach
+    public void setUp() {
+        dataSource.setUrl("jdbc:mysql://localhost:3306/employees?useUnicode=true");
+        dataSource.setUser("employees");
+        dataSource.setPassword("employees");
+
+        Flyway flyway = Flyway.configure().dataSource(dataSource).load();
+
+        flyway.clean();
+        flyway.migrate();
+
+    }
+
+    @Test
+    public void getPlayersOfTale() {
+        Set<PlayerOfTale> playersOfTale = new FrogPrince().getPlayersOfTale(dataSource, Tale.FROGPRINCE);
+
+        assertTrue(playersOfTale.contains(new PlayerOfTale("Békakirályfi", 27, Tale.FROGPRINCE)));
+        assertTrue(playersOfTale.contains(new PlayerOfTale("Kicsi királykisasszony", 21, Tale.FROGPRINCE)));
+        assertFalse(playersOfTale.contains(new PlayerOfTale("Hófehérke", 23, Tale.SNOWWHITE)));
+        assertFalse(playersOfTale.contains(new PlayerOfTale("Piroska", 9, Tale.LITTLEREDRIDINGHOOD)));
+    }
 
 }

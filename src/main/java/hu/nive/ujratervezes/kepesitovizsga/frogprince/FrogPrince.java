f4691a12afd0291e595da5c9d6ddafd86011201a
BirkasTivadar <birkastivadar@gmail.com>
2021-04-26 05:36:23 +0200

    FrogPrince kis javítás

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/frogprince/FrogPrince.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/frogprince/FrogPrince.java
@@ -1,39 +1,34 @@
 package hu.nive.ujratervezes.kepesitovizsga.frogprince;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.HashSet;
 import java.util.Set;
 
 public class FrogPrince {
 
     public Set<PlayerOfTale> getPlayersOfTale(DataSource dataSource, Tale tale) {
-        String sqlOrder = String.format("SELECT player_name, age, tale FROM tales WHERE tale='%s';", tale.name());
+
         Set<PlayerOfTale> result = new HashSet<>();
+
+        String sqlOrder = String.format("SELECT player_name, age, tale FROM tales WHERE tale='%s';", tale.name());
+
         try (
                 Connection conn = dataSource.getConnection();
-                PreparedStatement ps = conn.prepareStatement(sqlOrder)
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sqlOrder);
         ) {
-            processResultSet(result, ps);
-        } catch (SQLException sqlException) {
-            throw new IllegalStateException("Connection failed", sqlException);
-        }
-        return result;
-    }
 
-    private void processResultSet(Set<PlayerOfTale> result, PreparedStatement ps) {
-        try (ResultSet rs = ps.executeQuery()) {
             while (rs.next()) {
                 String name = rs.getString("player_name");
                 int age = rs.getInt("age");
                 String title = rs.getString("tale");
                 result.add(new PlayerOfTale(name, age, Tale.valueOf(title)));
             }
+
         } catch (SQLException sqlException) {
-            throw new IllegalStateException("Cannot query", sqlException);
+            throw new IllegalStateException("Connection failed", sqlException);
         }
+        return result;
     }
 }
